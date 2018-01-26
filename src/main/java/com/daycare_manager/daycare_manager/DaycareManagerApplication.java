d6246fe7f3ed6b20ab3755c61f415fc450ce61a1
terryhale <aggiefan17@yahoo.com>
2018-01-26 16:22:22 -0600

    first commit

---
+++ src/main/java/com/daycare_manager/daycare_manager/DaycareManagerApplication.java
@@ -0,0 +1,12 @@
+package com.daycare_manager.daycare_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaycareManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaycareManagerApplication.class, args);
+	}
+}
