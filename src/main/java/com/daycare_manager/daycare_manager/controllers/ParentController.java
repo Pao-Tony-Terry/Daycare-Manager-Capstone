67b63c7c41859a9ec3d9dd0dd9611af515143d6d
Johanna Gomez <pao_gomez1@hotmail.com>
2018-01-30 14:16:51 -0600

    ParentController shows the view of the form. Method for saving the record ready, pending changing enroll form to store information in its table.

--- src/main/java/com/daycare_manager/daycare_manager/controllers/ParentController.java
+++ src/main/java/com/daycare_manager/daycare_manager/controllers/ParentController.java
@@ -1,9 +1,34 @@
 package com.daycare_manager.daycare_manager.controllers;
 
 
+import com.daycare_manager.daycare_manager.daos.ChildrenRepository;
+import com.daycare_manager.daycare_manager.model.Child;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ParentController {
 
+    private ChildrenRepository childrenRepository;
+
+    public ParentController(ChildrenRepository childrenRepository) {
+        this.childrenRepository = childrenRepository;
+    }
+
+
+    @GetMapping("/parent/enroll")
+    public String showEnrollForm(Model viewModel) {
+        // new Child to catch the form to enroll one kid
+        viewModel.addAttribute("child", new Child());
+        return "users/enroll";
+    }
+
+    @PostMapping("/parent/enroll")
+    public String enrollAChild(@ModelAttribute Child child) {
+        childrenRepository.save(child);
+        return "redirect: /user/parent";
+    }
 }
