ae8707020f1dbe128abdca6180e8cbb07941f7c9
Johanna Gomez <pao_gomez1@hotmail.com>
2018-01-29 21:59:12 -0600

    general views included in homeController, not styled yet

---
+++ src/main/java/com/daycare_manager/daycare_manager/controllers/HomeController.java
@@ -0,0 +1,35 @@
+package com.daycare_manager.daycare_manager.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String welcome() {
+        return "home";
+    }
+
+
+    @GetMapping("/aboutus")
+    public String aboutUs() {
+        return "dcare_views/about_us";
+    }
+
+    @GetMapping("/gallery")
+    public String gallery() {
+        return "dcare_views/gallery";
+    }
+
+    @GetMapping("/staff")
+    public String staff() {
+        return "dcare_views/staff";
+    }
+
+    @GetMapping("/programs")
+    public String programsAndFees() {
+        return "dcare_views/programs_tuition_fees";
+    }
+
+}
