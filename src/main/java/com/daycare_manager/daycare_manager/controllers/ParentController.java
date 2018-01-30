7722dbe4a2fd4ae01ea9aae4dd78dc7df9b4c00e
Johanna Gomez <pao_gomez1@hotmail.com>
2018-01-30 15:47:56 -0600

    UsersRepository injected in ParentController to enroll a child.  Parent id is saved for the child but is still showing a mapping error

--- src/main/java/com/daycare_manager/daycare_manager/controllers/ParentController.java
+++ src/main/java/com/daycare_manager/daycare_manager/controllers/ParentController.java
@@ -2,7 +2,10 @@ package com.daycare_manager.daycare_manager.controllers;
 
 
 import com.daycare_manager.daycare_manager.daos.ChildrenRepository;
+import com.daycare_manager.daycare_manager.daos.UsersRepository;
 import com.daycare_manager.daycare_manager.model.Child;
+import com.daycare_manager.daycare_manager.model.User;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +17,17 @@ public class ParentController {
 
     private ChildrenRepository childrenRepository;
 
-    public ParentController(ChildrenRepository childrenRepository) {
-        this.childrenRepository = childrenRepository;
-    }
+    private UsersRepository usersRepository;
 
+//    public ParentController(ChildrenRepository childrenRepository) {
+//        this.childrenRepository = childrenRepository;
+//    }
+
+
+    public ParentController(ChildrenRepository childrenRepository, UsersRepository usersRepository) {
+        this.childrenRepository = childrenRepository;
+        this.usersRepository = usersRepository;
+    }
 
     @GetMapping("/parent/enroll")
     public String showEnrollForm(Model viewModel) {
@@ -28,7 +38,22 @@ public class ParentController {
 
     @PostMapping("/parent/enroll")
     public String enrollAChild(@ModelAttribute Child child) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        child.setParent(usersRepository.findOne(user.getId()));
         childrenRepository.save(child);
         return "redirect: /user/parent";
     }
+
+
+
+//    @PostMapping("posts/create")
+//    // we are not using pathvariable here because is comming from a form
+//    public String savePost(@ModelAttribute Post post){
+//        // this is coming form the db, and UserWithRoles is not mapped.
+//        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        post.setUser(usersDao.findOne(user.getId()));
+//        postService.save(post);
+//        return "redirect:/posts";
+//    }
+
 }
