1b0d1719dcdfba7ad63708fb0a98482dcf515224
Johanna Gomez <pao_gomez1@hotmail.com>
2018-01-31 11:13:43 -0600

    modal form created, not shown yet

--- src/main/java/com/daycare_manager/daycare_manager/controllers/ParentController.java
+++ src/main/java/com/daycare_manager/daycare_manager/controllers/ParentController.java
@@ -2,7 +2,6 @@ package com.daycare_manager.daycare_manager.controllers;
 
 
 import com.daycare_manager.daycare_manager.daos.ChildrenRepository;
-import com.daycare_manager.daycare_manager.daos.UsersRepository;
 import com.daycare_manager.daycare_manager.model.Child;
 import com.daycare_manager.daycare_manager.model.User;
 import com.daycare_manager.daycare_manager.services.UserService;
@@ -19,22 +18,36 @@ public class ParentController {
 
     private ChildrenRepository childrenRepository;
 
-    private UsersRepository usersRepository;
 
     private final UserService userService;
 
 
-    public ParentController(ChildrenRepository childrenRepository, UsersRepository usersRepository, UserService userService) {
+    public ParentController(ChildrenRepository childrenRepository, UserService userService) {
         this.childrenRepository = childrenRepository;
-        this.usersRepository = usersRepository;
         this.userService = userService;
     }
 
     @PostMapping("parent/edit")
     public String updateUser(@ModelAttribute User user){
         userService.update(user);
-        return "redirect:/posts";
+        return "redirect:/users/parent_profile";
     }
+
+
+
+    @GetMapping("parent/{id}/edit")
+    public String showEditForm(@PathVariable long id, Model viewModel){
+        User user = userService.findOne(id);
+        viewModel.addAttribute("user", user);
+        return "/users/edit_parent_profile";
+    }
+
+    @GetMapping("parent/{id}/delete")
+    public String deleteProfile(@PathVariable long id, Model viewModel){
+        userService.delete(id);
+        return "redirect:/home";
+    }
+
 
     @GetMapping("/parent/enroll")
     public String showEnrollForm(Model viewModel) {
@@ -48,7 +61,7 @@ public class ParentController {
     @PostMapping("/parent/enroll")
     public String enrollAChild(@ModelAttribute Child child) {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        child.setParent(usersRepository.findOne(user.getId()));
+        child.setParent(userService.findOne(user.getId()));
         childrenRepository.save(child);
         return "redirect:/user/parent";
     }
@@ -61,26 +74,6 @@ public class ParentController {
 
     }
 
-
-//    @GetMapping("posts/{id}/edit")
-//    public String showEditForm(@PathVariable long id, Model viewModel){
-//        Post post = postService.findOne(id);
-//        viewModel.addAttribute("post", post);
-//        return "/blog_template/edit";
-//    }
-//
-//    @GetMapping("posts/{id}/delete")
-//    public String deletePost(@PathVariable long id, Model viewModel){
-//        postService.delete(id);
-//        return "redirect:/posts";
-//    }
-//
-//
-//    @PostMapping("posts/edit")
-//    public String updatePost(@ModelAttribute Post post){
-//        postService.update(post);
-//        return "redirect:/posts";
-//    }
 
 
 
