463923b6b1c09835dba3ead1b9c7a6a2a0ea2e00
terryhale <aggiefan17@yahoo.com>
2018-02-01 09:35:22 -0600

    report card button on teacher profile is navigating to form

---
+++ src/main/java/com/daycare_manager/daycare_manager/services/ReportCardService.java
@@ -0,0 +1,23 @@
+//package com.daycare_manager.daycare_manager.services;
+//
+//import com.daycare_manager.daycare_manager.daos.UsersRepository;
+//import com.daycare_manager.daycare_manager.model.ReportCard;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class ReportCardService {
+//    private UsersRepository usersRepository;
+//
+//    public ReportCardService(UsersRepository usersRepository) {
+//        this.usersRepository = usersRepository;
+//    }
+//
+//    public ReportCard save(ReportCard reportCard) {
+//        usersRepository.save(reportCard);
+//        return reportCard;
+//    }
+//
+//    public void update(ReportCard reportCard) {
+//        usersRepository.save(reportCard);
+//    }
+//}
