5b667276e070663b23a8588b01b2b3859cd07384
terryhale <aggiefan17@yahoo.com>
2018-02-01 15:03:43 -0600

    commented out twilio

--- src/main/java/com/daycare_manager/daycare_manager/controllers/TwilioController.java
+++ src/main/java/com/daycare_manager/daycare_manager/controllers/TwilioController.java
@@ -11,29 +11,29 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TwilioController {
-
-    // Find your Account Sid and Token at twilio.com/user/account
-
-    @Value("${twilio.account.id}")
-    private String accountId;
-
-    @Value("${twilio.token.id}")
-    private String tokenId;
-
-    @Value("${twilio.number}")
-    private String twilioNumber;
-
-
-
-    @GetMapping("/testTwilio")
-    @ResponseBody
-    public String testingTwilio(){
-        Twilio.init(accountId, tokenId);
-        Message message = Message.creator(
-                new PhoneNumber("+12107082724"),  // this is my sprint number
-                new PhoneNumber("+12564748407"),
-                "This is the ship that made the Kessel Run in fourteen parsecs?").create();
-        return  message.getSid();
-    }
+//
+//    // Find your Account Sid and Token at twilio.com/user/account
+//
+//    @Value("${twilio.account.id}")
+//    private String accountId;
+//
+//    @Value("${twilio.token.id}")
+//    private String tokenId;
+//
+//    @Value("${twilio.number}")
+//    private String twilioNumber;
+//
+//
+//
+//    @GetMapping("/testTwilio")
+//    @ResponseBody
+//    public String testingTwilio(){
+//        Twilio.init(accountId, tokenId);
+//        Message message = Message.creator(
+//                new PhoneNumber("+12107082724"),  // this is my sprint number
+//                new PhoneNumber("+12564748407"),
+//                "This is the ship that made the Kessel Run in fourteen parsecs?").create();
+//        return  message.getSid();
+//    }
 }
 
