bb452ca978b3be3f5c873ec430cb1a1b48dd7e54
Johanna Gomez <pao_gomez1@hotmail.com>
2018-02-01 09:38:25 -0600

    testing twilio with other receiving numbers - not working yet

--- src/main/java/com/daycare_manager/daycare_manager/controllers/TwilioController.java
+++ src/main/java/com/daycare_manager/daycare_manager/controllers/TwilioController.java
@@ -33,28 +33,7 @@ public class TwilioController {
                 new PhoneNumber("+12107082724"),
                 new PhoneNumber("+12564748407"),
                 "This is the ship that made the Kessel Run in fourteen parsecs?").create();
-//        System.out.println(message.getSid());
         return  message.getSid();
     }
 }
 
-
-//import com.twilio.Twilio;
-//        import com.twilio.rest.api.v2010.account.Message;
-//        import com.twilio.type.PhoneNumber;
-//
-//public class Example {
-//    // Find your Account Sid and Token at twilio.com/user/account
-//    public static final String ACCOUNT_SID = "AC212127ad2e1affdd676c3554744fb6ee";
-//    public static final String AUTH_TOKEN = "your_auth_token";
-//
-//    public static void main(String[] args) {
-//        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-//
-//        Message message = Message.creator(new PhoneNumber("+15558675309"),
-//                new PhoneNumber("+15017250604"),
-//                "This is the ship that made the Kessel Run in fourteen parsecs?").create();
-//
-//        System.out.println(message.getSid());
-//    }
-//}
