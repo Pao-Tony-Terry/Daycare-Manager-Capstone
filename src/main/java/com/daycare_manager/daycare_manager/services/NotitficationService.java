58b7761e07b0faff51ab691f93493c203f677d6c
terryhale <aggiefan17@yahoo.com>
2018-02-02 16:41:14 -0600

    fixed twilio

--- src/main/java/com/daycare_manager/daycare_manager/services/NotitficationService.java
+++ src/main/java/com/daycare_manager/daycare_manager/services/NotitficationService.java
@@ -2,43 +2,45 @@ package com.daycare_manager.daycare_manager.services;
 
 
 //import com.daycare_manager.daycare_manager.daos.NotificationsRepository;
+import com.daycare_manager.daycare_manager.daos.NotificationsRepository;
 import com.daycare_manager.daycare_manager.daos.UsersRepository;
 //import com.daycare_manager.daycare_manager.model.Notification;
+import com.daycare_manager.daycare_manager.model.Notification;
 import com.daycare_manager.daycare_manager.model.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotitficationService {
-//
-//    private NotificationsRepository notificationsRepository;
-//
-//    public NotitficationService(NotificationsRepository notificationsRepository) {
-//        this.notificationsRepository = notificationsRepository;
-//    }
-//
-//
-//    public Iterable<Notification> findAll() {
-//        return notificationsRepository.findAll();
-//    }
-//
-//
-//
-//    public Notification save(Notification notification) {
-//        notificationsRepository.save(notification);
-//        return notification;
-//    }
-//
-//    public Notification findOne(long id) {
-//        return notificationsRepository.findOne(id);
-//
-//    }
-//
-//    public void update(Notification notification) {
-//        notificationsRepository.save(notification);
-//    }
-//
-//    public void delete(long id) {
-//        notificationsRepository.delete(id);
-//
-//    }
+
+    private NotificationsRepository notificationsRepository;
+
+    public NotitficationService(NotificationsRepository notificationsRepository) {
+        this.notificationsRepository = notificationsRepository;
+    }
+
+
+    public Iterable<Notification> findAll() {
+        return notificationsRepository.findAll();
+    }
+
+
+
+    public Notification save(Notification notification) {
+        notificationsRepository.save(notification);
+        return notification;
+    }
+
+    public Notification findOne(long id) {
+        return notificationsRepository.findOne(id);
+
+    }
+
+    public void update(Notification notification) {
+        notificationsRepository.save(notification);
+    }
+
+    public void delete(long id) {
+        notificationsRepository.delete(id);
+
+    }
 }
